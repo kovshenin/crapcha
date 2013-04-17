9075a4ad08ae299fec5617183cfa2fbfcd8f1f1f
Konstantin Kovshenin <kovshenin@gmail.com>
2013-04-17 11:39:58 +0400

    First version

---
+++ crapcha.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: Crapcha
+ * Description: Protect your WordPress site with a completely ridiculous and phony captcha that hassles for amusement.
+ * @see http://crapcha.com/
+ */
+class Crapcha_Plugin {
+	function __construct() {
+		add_action( 'login_form', array( $this, 'login_form' ) );
+	}
+
+	function login_form() {
+		?>
+		<style>
+			#login { width: 358px; }
+		</style>
+		<p>
+			<label>Security</label>
+			<iframe src="http://crapcha.com/embed/" frameborder="0" scrolling="0" width="300" height="150" style="margin-top: 4px;"></iframe>
+		</p>
+		<?php
+	}
+}
+new Crapcha_Plugin;
